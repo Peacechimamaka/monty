8b03e68d71a1205a2a66567d5c4ed1dee6623e0d
Peace Chimamaka <peaceamaka111@gmail.com>
2024-01-19 22:09:15 -0800

    monty

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <monty.h>
 
 /**
- *
- *
- *
+ *main - the function main for the stack
+ *@ac: arguement count
+ *@av:aguement vector
+ *Return: 0
  */
 
 int main (int ac char *av[])
@@ -27,3 +28,41 @@ int main (int ac char *av[])
 		fprint(stderr, "Error: Can't open file %s\n", av[1]);
 		exit (EXIT_FAILURE);
 	}
+
+	rd = getline(&input, &size, mfile);
+	while (rd != -1)
+	{
+		count++;
+		code = strtok(input, " \n\t");
+		if (code)
+		{
+			if (strcmp(code, "pint") == 0)
+				pint(&stack, count);
+			else if (strcmp(code, "push") == 0)
+			{
+				output = strtok(NULL, " \t\n");
+				push(&stack, count);
+			}
+			else if (strcmp(code, "pall") == 0)
+				pall(&stack, count);
+			else if (strcmp(code, "pop") == 0)
+				pop(&stack, count);
+			else if (strcmp(code, "swap") == 0)
+				swap(&stack, count);
+			else if (strcmp(code, "add") == 0)
+				add(&stack, count);
+			else if (strcmp(code, "nop") == 0)
+				add(&stack, count);
+			else
+			{
+				fprint(stderr, "L%d: unknown instruction %s\n", count code);
+				free(input);
+				fclose(mfile);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	free(input);
+	fclose(mfile);
+	exit(EXIT_SUCCESS);
+}
